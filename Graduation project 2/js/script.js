ab7114f426f2fc1dd3e6c474b20a35e69750db37
venyasmagenka <Aporohov777@gmail.com>
2025-11-19 00:10:54 +0300

    Delete Graduation project 2 directory

--- Graduation project 2/js/script.js
+++
@@ -1,8 +0,0 @@
-document.querySelector('.switch__input').addEventListener('change', function(){
-  if (this.checked){
-    window.scrollBy({
-      top:1100,
-      behavior: 'smooth'
-    });
-  }
-})
