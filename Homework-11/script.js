7714e7249381c808c07202c2f8e94966ee2272a3
venyasmagenka <Aporohov777@gmail.com>
2025-11-19 10:46:59 +0300

    Add files via upload

---
+++ Homework-11/script.js
@@ -0,0 +1,26 @@
+const btn=document.querySelector('.btn--open')
+const modal=document.querySelector('.modal')
+const body=document.querySelector('.body')
+
+const closeModal=() =>{
+  modal.classList.remove('modal--open')
+  body.classList.remove('body--fixed')
+}
+const openModal=() =>{
+  modal.classList.add('modal--open')
+  body.classList.add('body--fixed')
+}
+
+btn.addEventListener('click', ()=>{
+  openModal()
+})
+modal.addEventListener('click' , event =>{
+  if (event.target && event.target.classList.contains('modal') || event.target.classList.contains('modal__window-close')){
+    closeModal()
+  }
+})
+document.addEventListener('keydown', event=>{
+  if(event.code==='Escape'){
+    closeModal()
+  }
+})
